99ff59225ee6c351b9017b11fd53132291a2a22b
Nikolaj Hoggins Jacobsen <nikolajhjacobsen@gmail.com>
2019-10-28 11:46:38 +0100

    Mail build
    Errors tho

--- Gruppe3/Functions.cs
+++ Gruppe3/Functions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Mail;
 
 namespace Gruppe3
 {
@@ -6,7 +7,19 @@ namespace Gruppe3
     {
         public static bool SendEmail()
         {
-            return false;
+            string username = "niko9743@gmail.com";
+            SmtpClient mailClient = MailController.CreateConnection(username, "olivia75");
+            try
+            {
+                mailClient.Send(MailController.CreateMail(username, "mollevip@gmail.com", "Dinmor", "Yeet nibba"));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            
         }
     }
 }
