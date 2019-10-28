f533ee262fa3448bb68e79339ed3ed5bc2186599
Jeppe <jepnyvang31@gmail.com>
2019-10-28 11:16:32 +0100

    Et eller andet lort

--- Gruppe3/Program.cs
+++ Gruppe3/Program.cs
@@ -8,5 +8,18 @@ namespace Gruppe3
         {
             Console.WriteLine("Hello World!");
         }
+
+        static string Emails()
+        {
+            Console.WriteLine("Sign up for emails with your classroom");
+            Console.Write("Enter name: ");
+            string nameToLower = Console.ReadLine().ToLower();
+            Console.Write("Enter email: " );
+            string email = Console.ReadLine();
+
+
+            return nameToLower;
+        }
+
     }
 }
