475d936ba1e7abadba9348304fde2e798f343917
Jens <jens.e.bouwens@gmail.com>
2019-10-28 12:08:43 +0100

    file stuff
    if the file paths don't work i will need to know.

--- Gruppe3/Functions.cs
+++ Gruppe3/Functions.cs
@@ -24,12 +24,14 @@ namespace Gruppe3
 
         public static void WriteInfo(string[] info)
         {
-            
+            using System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"Gruppe3\Gruppe3\UserInfo.text", true); // define the folder and file path.
+            file.WriteLine(info[0], info[1], info[3]); // write to the file.
         }
 
         public static Array UserInfo()
         {
-            string[] output = new string[3];
+            string[] output = System.IO.File.ReadAllLines(@"Gruppe3\Gruppe3\UserInfo.text"); // read all lines and put them in a new string called output.
             return output;
         }
     }
