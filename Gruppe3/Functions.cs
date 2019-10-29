213e382b888926964daedb6d2851f3f78a8ebe9b
Jens <jens.e.bouwens@gmail.com>
2019-10-29 12:53:39 +0100

    Post request.
    please if you don't need this delete it

--- Gruppe3/Functions.cs
+++ Gruppe3/Functions.cs
@@ -55,5 +55,20 @@ namespace Gruppe3
             var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
             return responseString;
         }
+
+        public static string HttpPostRequest() // if this is not what you were looking for dellet it.
+        {
+            var values = new Dictionary<string, string>
+            {
+            { "thing1", "hello" },
+            { "thing2", "world" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+        }
     }
 }
