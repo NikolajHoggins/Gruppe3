2fcfb29872e90236e7acddd8f8a8ce9a94e2015f
Nikolaj Hoggins Jacobsen <nikolajhjacobsen@gmail.com>
2019-10-30 13:54:23 +0100

    visual studio cutoff

--- Gruppe3/Functions.cs
+++ Gruppe3/Functions.cs
@@ -9,24 +9,6 @@ namespace Gruppe3
 {
     class Functions
     {
-        public static bool SendEmail(string reciever, string subject, string body = "")
-        {
-            string username = "niko9743@gmail.com";
-            string password = "olivia75";
-
-            SmtpClient mailClient = MailController.CreateConnection(username, password);
-            try
-            {
-                mailClient.Send(MailController.CreateMail(username, reciever, subject, body));
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-            
-        }
         public static string AddUser(string[] info)
         {
             string requesturl = $"https://nikolaj.bricksite.net/adduser/{info[0]}/{info[1]}/{info[2]}";
@@ -45,7 +27,5 @@ namespace Gruppe3
 
             return html;
         }
-
-        //public static void S
     }
 }
