dbace2a40bbd6b4670c6c97a2767eee38c8f8758
Jens <jens.e.bouwens@gmail.com>
2019-10-30 12:05:03 +0100

    removed some unused line's

--- Gruppe3/Functions.cs
+++ Gruppe3/Functions.cs
@@ -68,21 +68,6 @@ namespace Gruppe3
             
             return html;
         }
-        /*public static string HttpPostRequest() // if this is not what you were looking for dellet it.
-        {
-            var values = new Dictionary<string, string>
-            {
-            { "thing1", "hello" },
-            { "thing2", "world" }
-            };
-
-            var content = new FormUrlEncodedContent(values);
-
-            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
-
-            var responseString = await response.Content.ReadAsStringAsync();
-        }*/
-
         //public static void S
     }
 }
